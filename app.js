4ada774584e0d992419c0204081a2cf36e3877d2
Jose Alcaraz <jalcaraz007@gmail.com>
2019-04-07 10:28:46 -0700

    Week 2 workshop assingment

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-var app = express();
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
+
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
+
+
 connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+  	console.log('Connected correctly to server');
 
+  	}, (err) => {console.log(err);
+});
+var app = express();
 
-// view engine setup
-app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -36,21 +36,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
-  next(createError(404));
+  	next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
