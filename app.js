55ac36b5007b0e286321fc48e4b1a7315e6e4be3
Jose Alcaraz <jalcaraz007@gmail.com>
2019-04-11 17:53:23 -0700

    Basic Authentication

--- app.js
+++ app.js
@@ -56,4 +56,28 @@ app.use(function(err, req, res, next) {
 	res.render('error');
 });
 
-module.exports = app;
+function auth (req, res, next) {
+	console.log(req.headers);
+	var authHeader = req.headers.authorization;
+	if (!authHeader) {
+		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		next(err);
+		return;
+	}
+  
+	var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+	var user = auth[0];
+	var pass = auth[1];
+	if (user == 'admin' && pass == 'password') {
+		next(); // authorized
+	} else {
+		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');      
+		err.status = 401;
+		next(err);
+	}
+  }
+
+  app.use(auth);
